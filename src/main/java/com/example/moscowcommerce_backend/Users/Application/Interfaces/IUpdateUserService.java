3ec32278de0c25eca032737ef92cb100a6b7899b
Nicolas Ludueña <nicolascluduena@gmail.com>
2024-09-20 21:00:39 -0300

    Add interfaces for updating user information and corresponding DTO class

---
+++ src/main/java/com/example/moscowcommerce_backend/Users/Application/Interfaces/IUpdateUserService.java
@@ -0,0 +1,7 @@
+package com.example.moscowcommerce_backend.Users.Application.Interfaces;
+
+import com.example.moscowcommerce_backend.Users.Insfraestructure.DTO.ResultUserDTO;
+
+public interface IUpdateUserService {
+    ResultUserDTO update();
+}
---
+++ src/main/java/com/example/moscowcommerce_backend/Users/Insfraestructure/DTO/UpdateUserDTO.java
@@ -0,0 +1,27 @@
+package com.example.moscowcommerce_backend.Users.Insfraestructure.DTO;
+
+import jakarta.validation.constraints.Email;
+import jakarta.validation.constraints.NotBlank;
+import jakarta.validation.constraints.Pattern;
+import jakarta.validation.constraints.Size;
+import lombok.Data;
+
+@Data
+public class UpdateUserDTO {
+    @NotBlank(message = "El id no puede estar vacio")
+    private Integer id;
+    
+    @Email(message = "El email no es valido")
+    @Size(min = 5, max = 50, message = "El email debe tener entre 5 y 50 caracteres")
+    private String email;
+    
+    @Size(min = 1, max = 50, message = "El nombre debe tener entre 1 y 50 caracteres")
+    private String fullName;
+    
+    @Size(min = 8, max = 50, message = "La contraseña debe tener entre 8 y 50 caracteres")
+    @Pattern(regexp = "^(?=.*[a-z])(?=.*[A-Z])(?=.*\\d).*$", message = "La contraseña debe tener al menos una letra mayúscula, una letra minúscula y un número")
+    private String password;
+    
+    @Pattern(regexp = "^(CUSTOMER|ADMIN)$", message = "El rol debe ser CUSTOMER o ADMIN (mayúsculas)")
+    private String role;
+}
